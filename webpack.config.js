ae93d3ae5baad68d0b7a8e3880adbce5b0224305
Palash Mondal <palashm@live.in>
2018-01-25 20:46:18 +0530

    Tree Shaking - Minify the Output

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,14 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-	entry: {
-		app: './src/index.js'
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		contentBase: path.join(__dirname, 'dist')
-	},
+	entry: './src/index.js',
 	output: {
-		filename: '[name].bundle.js',
+		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist')
 	},
-	plugins: [
-		new CleanWebpackPlugin(['dist']),
-		new HtmlWebpackPlugin({
-			title: 'Webpack - Hot Module Replacement'
-		}),
-		new webpack.NamedModulesPlugin(),
-		new webpack.HotModuleReplacementPlugin()
-	]
+	devServer: {
+		contentBase: path.resolve(__dirname, 'dist')
+	},
+	plugins: [new UglifyJSPlugin()]
 };
